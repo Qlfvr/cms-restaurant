bf17c5ef223c5625422ae7b897fe92720e208f22
Quentin <lefevre.quentin@gmail.com>
2020-04-21 11:18:58 +0200

    complete single page template

--- themes/restaurantqlfvr/single.php
+++ themes/restaurantqlfvr/single.php
@@ -1,1 +1,18 @@
-<?php
+<?php get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+            <?php
+            /* Start the Loop */
+            while (have_posts()) :
+                the_post();
+                get_template_part('template-parts/content/content', 'single');
+            endwhile; // End of the loop.
+
+            get_template_part('template-parts/modules/module', 'recipes'); ?>
+
+        </main><!-- .site-main -->
+    </div><!-- .content-area -->
+
+<?php get_footer(); ?>
